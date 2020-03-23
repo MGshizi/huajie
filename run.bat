8d38bcd9a59925ced9686f55d0f46d0eda0eb625
MGshizi <mgshizi@163.com>
2020-03-23 23:16:27 +0800

    website update

--- run.bat
+++ run.bat
@@ -2,13 +2,5 @@ cd ./jemdoc
 makefile
 cd ..
 git add *
-git add *html
-git add *css
-git add contest
-git add jemdoc
-git add ref
-git add image
-git add run.bat
-git add image
 git commit -m "update webpage"
 git push
